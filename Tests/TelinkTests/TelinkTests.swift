3e0f67b35aab8781cb3f7d84161170aa956529cb
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-09 03:32:53 -0500

    Updated unit tests

--- Tests/TelinkTests/TelinkTests.swift
+++ Tests/TelinkTests/TelinkTests.swift
@@ -2,11 +2,25 @@ import XCTest
 @testable import Telink
 
 final class TelinkTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func testSerialPortProtocolMessageEncoding() throws {
+        let encoder = TelinkEncoder()
+        let data = Data(hexadecimal: "55AA0009FFF6DD0B1518DB")!
+        let command = SerialPortProtocolMessage(
+            type: 0x55AA,
+            length: 9,
+            payload: Data(hexadecimal: "FFF6DD0B1518DB")!
+        )
+        let encodedData = try encoder.encode(command)
+        XCTAssertEqual(data, encodedData)
+    }
+    
+    func testSerialPortProtocolMessageDecoding() throws {
+        let decoder = TelinkDecoder()
+        let data = Data(hexadecimal: "55AA0008FFF7DD0B00D6")!
+        let event = try decoder.decode(SerialPortProtocolMessage.self, from: data)
+        XCTAssertEqual(event.type, 0x55AA)
+        XCTAssertEqual(event.length, 8)
+        XCTAssertEqual(event.payload, Data(hexadecimal: "FFF7DD0B00D6"))
     }
 }
