48c566cd2908cf2e50c283dc68cf677ea10a8920
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-09 04:54:37 -0500

    Add `CentralManager.sendSerialPortProtocol()`

--- Sources/Telink/SerialPortProtocol/SerialPortCommand.swift
+++ Sources/Telink/SerialPortProtocol/SerialPortCommand.swift
@@ -6,11 +6,15 @@
 //
 
 import Foundation
+import Bluetooth
+import GATT
 
 public protocol SerialPortProtocolCommand {
     
     static var type: SerialPortProtocolType { get }
 }
+
+// MARK: - Encoding
 
 public extension SerialPortProtocolMessage {
     
@@ -24,5 +28,21 @@ public extension SerialPortProtocolMessage {
             length: UInt16(payload.count),
             payload: payload
         )
+    }
+}
+
+// MARK: - Central
+
+public extension CentralManager {
+    
+    /// Send a Telink SPP command.
+    func sendSerialPortProtocol<Command>(
+        command: Command,
+        characteristic: Characteristic<Self.Peripheral, Self.AttributeID>
+    ) async throws where Command: SerialPortProtocolCommand, Command: Encodable {
+        assert(characteristic.uuid == .telinkSerialPortProtocolCommand)
+        let message = try SerialPortProtocolMessage(command: command)
+        let data = try message.encode()
+        try await self.writeValue(data, for: characteristic, withResponse: false)
     }
 }
