1dda4ac074f12a61a6a6d901c1c29ba6db2b92ed
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-15 16:30:45 -0400

    Add `TelinkAdvertisement` decoding

--- Sources/Telink/Advertisement.swift
+++ Sources/Telink/Advertisement.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 import Bluetooth
+import GATT
 
+/// Telink Advertisement
 public struct TelinkAdvertisement: Equatable, Hashable, Codable, Sendable {
     
     public let name: String
@@ -22,5 +24,41 @@ public extension TelinkAdvertisement {
         public let vendor: Bluetooth.CompanyIdentifier
         
         public let address: TelinkAdvertisement.Address
+    }
+}
+
+public extension TelinkAdvertisement {
+    
+    init?(name: String, manufacturerData: GATT.ManufacturerSpecificData) {
+        guard let manufacturerDataValue = TelinkAdvertisement.ManufacturerData(manufacturerData: manufacturerData)
+            else { return nil }
+        self.name = name
+        self.manufacturerData = manufacturerDataValue
+    }
+    
+    init?<T: GATT.AdvertisementData>(_ advertisement: T) {
+        guard let manufacturerData = advertisement.manufacturerData,
+              let name = advertisement.localName
+            else { return nil }
+        self.init(name: name, manufacturerData: manufacturerData)
+    }
+}
+
+public extension TelinkAdvertisement.ManufacturerData {
+    
+    static var companyIdentifier: CompanyIdentifier { .telinkSemiconductor }
+    
+    internal static var decoder: TelinkDecoder {
+        TelinkDecoder(isLittleEndian: true)
+    }
+    
+    init?(manufacturerData: GATT.ManufacturerSpecificData) {
+        guard manufacturerData.companyIdentifier == Self.companyIdentifier else {
+            return nil
+        }
+        guard let value = try? Self.decoder.decode(TelinkAdvertisement.ManufacturerData.self, from: manufacturerData.additionalData) else {
+            return nil
+        }
+        self = value
     }
 }
