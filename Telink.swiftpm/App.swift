e1e9fa6fc520621a1bad302d682017e7fcca7224
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-15 16:48:36 -0400

    Add Swift Playground app

---
+++ Telink.swiftpm/App.swift
@@ -0,0 +1,17 @@
+import Foundation
+import SwiftUI
+import CoreBluetooth
+import Bluetooth
+import GATT
+import Telink
+
+@main
+struct TelinkApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(Store.shared)
+        }
+    }
+}
